a003a52546ff8a61a344a87f21a032902f704b60
Naru-hub <1208ny0324@gmail.com>
2022-08-31 23:43:34 +0900

    +ボタンを押すとincrementが反映される処理を追加

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { increment } from "./redux/counterSlice";
 
 function App() {
   // 状態にアクセスするためのHooks
   const count = useSelector((state) => state.counter.value);
+  // ActionをStoreに通知するためのHooks
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>＋</button>
     </div>
   );
 }

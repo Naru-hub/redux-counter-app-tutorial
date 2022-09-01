1e957be61ca4c3c6cef2cdf321c10da4b09dfaf5
Naru-hub <1208ny0324@gmail.com>
2022-09-01 21:02:03 +0900

    formに入力した値の数値を足していく処理を追加

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { decrement, increment } from "./redux/counterSlice";
+import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
 
 function App() {
   // 状態にアクセスするためのHooks
   const count = useSelector((state) => state.counter.value);
   // ActionをStoreに通知するためのHooks
   const dispatch = useDispatch();
+
+  // 入力された値の分をたすための入力値の監視
+  const [incrementAmount, setIncrementAmount] = useState("2");
   return (
     <div className="App">
       <h1>count: {count}</h1>
+      <input
+        onChange={(e) => setIncrementAmount(e.target.value)}
+        value={incrementAmount}
+      />
       <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(decrement())}>−</button>
+      <button
+        onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}
+      >
+        追加
+      </button>
     </div>
   );
 }
